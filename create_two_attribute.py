007d3810637d4b51ecfbd465bf9075cf25bd1198
Umidjon12346 <umidshari456@gmail.com>
2023-11-16 17:08:15 +0500

    done

--- create_two_attribute.py
+++ create_two_attribute.py
@@ -1,3 +1,6 @@
 #Create a "Person" class
-
+class Person:
+    def __init__(self,name,age):
+        self.age = age
+        self.name = name
 #that has a name("name") and a age("age")
--- create_two_attribute_one_objects.py
+++ create_two_attribute_one_objects.py
@@ -1,3 +1,7 @@
 from create_two_attribute import Person
-
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age  = age
+p1 = Person("Ali",25)
 #create an object named "person" whose name is "Ali", age is "25"
